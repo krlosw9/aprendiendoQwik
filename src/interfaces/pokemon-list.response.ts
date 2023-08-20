40fa1513aa79c919473eac835266cbb0771fed73
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-08-20 10:12:03 -0500

    Creadas interfaces, relizada peticion http y renderizada la lista de nombres de pokemon

---
+++ src/interfaces/pokemon-list.response.ts
@@ -0,0 +1,12 @@
+
+export interface PokemonListResponse {
+  count:    number;
+  next:     string;
+  previous: string;
+  results:  BasicPokemonInfo[]
+}
+
+export interface BasicPokemonInfo {
+  name: string;
+  url:  string;
+}
--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,9 +1,48 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { Link, type DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
+import type { BasicPokemonInfo } from "~/interfaces";
+
+export const usePokemonList = routeLoader$<BasicPokemonInfo[]>(async() =>{
+    
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`);
+  const data = await res.json();
+
+  return data.results;
+});
 
 export default component$(() => {
+  const pokemons = usePokemonList();
+  
   return (
-    <>pokemons / list-ssr</>
+    <>
+      <div class="flex flex-col">
+        <span class="my-5 text-5xl">Status</span>
+        <span>Offset: xxxx</span>
+        <span>Está cargando página: xxxx</span>
+      </div>
+
+      <div class="mt-10">
+        <Link href={`/pokemons/list-ssr/?offset=10`}
+          class="btn btn-primary mr-2">
+          Anterior
+        </Link>
+        <Link href={`/pokemons/list-ssr/?offset=20`} 
+          class="btn btn-primary">
+          Siguiente
+        </Link>
+      </div>
+
+      <div class="grid grid-cols-6 mt-5">
+        {
+          pokemons.value.map(({name}) => (
+            <div key={name} class="m-5 flex flex-col justify-center items-center">
+              <span class="capitalize">{name}</span>
+            </div>
+          ))
+        }
+        
+      </div>
+    </>
   )
 })
 
