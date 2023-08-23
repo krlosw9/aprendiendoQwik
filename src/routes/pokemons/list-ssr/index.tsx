7826aa938812d50befb12f406c4a29ef76d37546
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-08-22 21:24:37 -0500

    Query Parameters

--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,10 +1,13 @@
-import { component$ } from "@builder.io/qwik";
-import { Link, type DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
+import { component$, useComputed$ } from "@builder.io/qwik";
+import { Link, type DocumentHead, routeLoader$, useLocation } from "@builder.io/qwik-city";
 import type { BasicPokemonInfo } from "~/interfaces";
 
-export const usePokemonList = routeLoader$<BasicPokemonInfo[]>(async() =>{
-    
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`);
+export const usePokemonList = routeLoader$<BasicPokemonInfo[]>(async({query,pathname, redirect}) =>{
+  
+  const offset = Number( query.get('offset') || '0' );
+  if ( (isNaN(offset)) || ( offset < 0 ) ) throw redirect(301, pathname );
+  
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`);
   const data = await res.json();
 
   return data.results;
@@ -12,21 +15,28 @@ export const usePokemonList = routeLoader$<BasicPokemonInfo[]>(async() =>{
 
 export default component$(() => {
   const pokemons = usePokemonList();
-  
+  const location = useLocation();
+  const quantityPerPage = 10;
+
+  const currentOffset = useComputed$<number>(() => {
+    const offsetString = new URLSearchParams( location.url.search );
+    return Number(offsetString.get('offset') || 0 );
+  })
+
   return (
     <>
       <div class="flex flex-col">
         <span class="my-5 text-5xl">Status</span>
-        <span>Offset: xxxx</span>
+        <span>Offset: { currentOffset }</span>
         <span>Está cargando página: xxxx</span>
       </div>
 
       <div class="mt-10">
-        <Link href={`/pokemons/list-ssr/?offset=10`}
+        <Link href={ `/pokemons/list-ssr/?offset=${ currentOffset.value - quantityPerPage }` }
           class="btn btn-primary mr-2">
           Anterior
         </Link>
-        <Link href={`/pokemons/list-ssr/?offset=20`} 
+        <Link href={ `/pokemons/list-ssr/?offset=${ currentOffset.value + quantityPerPage }` }
           class="btn btn-primary">
           Siguiente
         </Link>
