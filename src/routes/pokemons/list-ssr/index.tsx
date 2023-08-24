aa72b15c17092e7a8fd7db473e9889877a017275
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-08-23 21:46:28 -0500

    Query Parameters

--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,32 +1,47 @@
-import { component$ } from "@builder.io/qwik";
-import { Link, type DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
-import type { BasicPokemonInfo } from "~/interfaces";
+import { component$, useComputed$ } from "@builder.io/qwik";
+import { Link, type DocumentHead, routeLoader$, useLocation } from "@builder.io/qwik-city";
+import { type BasicPokemonInfo } from "~/interfaces";
 
-export const usePokemonList = routeLoader$<BasicPokemonInfo[]>(async() =>{
-    
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`);
-  const data = await res.json();
+export const usePokemonList = routeLoader$<BasicPokemonInfo[]>(async ({query, redirect, pathname}) =>{
+  const offset = Number(query.get('offset'))
+
+  if (isNaN(offset) || offset < 0) throw redirect(301, pathname)
+  
+  const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`);
+  const data = await resp.json();
 
   return data.results;
-});
+})
 
 export default component$(() => {
   const pokemons = usePokemonList();
-  
+  const location = useLocation();
+
+  const currentOffset = useComputed$(() =>{
+    const offsetString = new URLSearchParams(location.url.search);
+    return Number(offsetString.get('offset'));
+  })
+
+  const handlerUrl = (action: number) =>{
+    if(action === -10 && currentOffset.value <= 0) return 
+
+    return `/pokemons/list-ssr/?offset=${currentOffset.value+action}`
+  }
+
   return (
     <>
       <div class="flex flex-col">
         <span class="my-5 text-5xl">Status</span>
-        <span>Offset: xxxx</span>
-        <span>Está cargando página: xxxx</span>
+        <span>Offset: {currentOffset.value}</span>
+        <span class='h-4'> {location.isNavigating ? 'Cargando...' : ''}</span>
       </div>
 
       <div class="mt-10">
-        <Link href={`/pokemons/list-ssr/?offset=10`}
+        <Link href={handlerUrl(-10)}
           class="btn btn-primary mr-2">
           Anterior
         </Link>
-        <Link href={`/pokemons/list-ssr/?offset=20`} 
+        <Link href={handlerUrl(+10)} 
           class="btn btn-primary">
           Siguiente
         </Link>
@@ -34,13 +49,13 @@ export default component$(() => {
 
       <div class="grid grid-cols-6 mt-5">
         {
-          pokemons.value.map(({name}) => (
+          pokemons.value.map(({name}) =>(
             <div key={name} class="m-5 flex flex-col justify-center items-center">
               <span class="capitalize">{name}</span>
             </div>
           ))
+          
         }
-        
       </div>
     </>
   )
