251a0f452d87a196375a3d9e503065761a138b4b
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-12-10 10:47:46 -0500

    Feat: Creadas ruta login y dashboard

---
+++ src/routes/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { Slot, component$ } from "@builder.io/qwik";
+
+
+export default component$(() => {
+  return (
+    <>
+      <h3>Layout Auth</h3>
+      <Slot /> 
+    </>
+  )
+})
---
+++ src/routes/dashboard/index.tsx
@@ -0,0 +1,15 @@
+import { component$ } from "@builder.io/qwik";
+
+
+export default component$(() => {
+  return (
+    <>
+      <h3>
+        Admin Dashboard
+      </h3>
+      <span>
+      Esta ruta debe ser protegida...
+      </span>
+    </>
+  )
+})
