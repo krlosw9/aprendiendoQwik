9f4e03ea7e3aeaed0cd06a748b3fabfb6c2717b7
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-12-13 20:16:35 -0500

    Estilos css del login

--- src/routes/(auth)/layout.tsx
+++ src/routes/(auth)/layout.tsx
@@ -1,11 +1,28 @@
-import { Slot, component$ } from "@builder.io/qwik";
+import { Slot, component$, useStylesScoped$ } from '@builder.io/qwik';
+import styles from './auth-layout.css?inline';
 
-
+// https://freefrontend.com/tailwind-forms/
 export default component$(() => {
-  return (
-    <>
-      <h3>Layout Auth</h3>
-      <Slot /> 
-    </>
-  )
-})
+
+    useStylesScoped$(styles);
+
+    return (
+        <div class="login-main">
+            <div class="login-bg">
+                <div
+                    class="login-bg-gradient">
+                </div>
+                <div class="login-white-bg">
+                    <div class="max-w-md mx-auto">
+                        <div>
+                            <h1 class="login-title">Login Form</h1>
+                        </div>
+                        <div class="login-content">
+                            <Slot />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+});
