c6f42c00b7f768406c8b173615ce4df22b047519
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-12-13 21:44:53 -0500

    Formulario tradicional basico

--- src/routes/(auth)/login/index.tsx
+++ src/routes/(auth)/login/index.tsx
@@ -1,29 +1,43 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { $, component$, useStore, useStylesScoped$ } from '@builder.io/qwik';
 
 import styles from './login.css?inline';
 
 export default component$(() => {
 
-    useStylesScoped$(styles);
+  useStylesScoped$(styles);
 
-    return (
-        <form class="login-form">
-            <div class="relative">
-                <input name="email" type="text" placeholder="Email address" />
-                <label for="email">Email Address</label>
-            </div>
-            <div class="relative">
-                <input id="password" name="password" type="password" placeholder="Password" />
-                <label for="password">Password</label>
-            </div>
-            <div class="relative">
-                <button>Ingresar</button>
-            </div>
+  const formState = useStore({
+    email: '',
+    password: ''
+  });
+
+  const onSubmit = $(() => {
+    const { email, password } = formState;
+    console.log(email, password);
+  })
+
+  return (
+    <form onSubmit$={onSubmit} class="login-form" preventdefault:submit>
+      <div class="relative">
+        <input 
+          onInput$={(ev) => formState.email = (ev.target as HTMLInputElement).value }
+          name="email" type="text" placeholder="Email address" />
+        <label for="email">Email Address</label>
+      </div>
+      <div class="relative">
+        <input 
+          onInput$={(ev) => formState.password = (ev.target as HTMLInputElement).value }
+          id="password" name="password" type="password" placeholder="Password" />
+        <label for="password">Password</label>
+      </div>
+      <div class="relative">
+        <button>Ingresar</button>
+      </div>
 
 
-            {/* <code>
-                { JSON.stringify( formState, undefined , 2 ) }
-            </code> */}
-        </form>
-    )
+      <code>
+        { JSON.stringify( formState, undefined , 2 ) }
+      </code>
+    </form>
+  )
 });
