f3d764798cb555361ce4c1bc1c3ed8da65a4ab91
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-12-19 22:24:39 -0500

    Proteccion de rutas

--- src/routes/dashboard/layout.tsx
+++ src/routes/dashboard/layout.tsx
@@ -1,5 +1,17 @@
 import { Slot, component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
 import Navbar from "~/components/shared/navbar/navbar";
+
+export const useCheckAuthCookie = routeLoader$(({cookie, redirect}) => {
+  const jwtCookie = cookie.get('jwt');
+  //Validamos en el backend con jwt si realmente es el token
+  if(jwtCookie){
+    console.log(`cookie value: ${jwtCookie}`);
+    return;
+  }
+  
+  redirect(302, '/login');
+})
 
 export default component$(() => {
   return (
